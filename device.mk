7eb618a029b23889bb5fcf3b34ca518eec417f7a
DarkJoker360 <simoespo159@gmail.com>
2019-05-31 10:49:53 +0200

    import oreo nfc libs
    Signed-off-by: DarkJoker360 <simoespo159@gmail.com>

--- device.mk
+++ device.mk
@@ -55,6 +55,18 @@ PRODUCT_PACKAGES += \
     com.android.nfc_extras \
     nfc_nci.pn54x.default
 
+PRODUCT_COPY_FILES += \
+    device/huawei/prague/nfc/libnfc_brcm_pra_L31.conf:system/etc/libnfc-brcm.conf \
+    device/huawei/prague/nfc/libnfc-nci.conf:system/etc/libnfc-nci.conf \
+    device/huawei/prague/nfc/libnfc_nxp_pra_L31.conf:system/etc/libnfc-nxp.conf \
+    device/huawei/prague/nfc/nfcscc_access.xml:system/etc/nfcscc_access.xml \
+    device/huawei/prague/nfc/nfcse_access.xml:system/etc/nfcse_access.xml \
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
